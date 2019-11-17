36cd3adfc37708d20bc8bfc55dbe1cea3c92bdb3
darekcharkot <darek.charkot@gmail.com>
2019-11-17 10:26:09 +0100

    Implemented partials with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,11 +28,12 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
+    3.times { @portfolio_item.technologies.build }
   end
 
   def update
     respond_to do |format|
-      if @portfolio.update(portfolio_item_params)
+      if @portfolio_item.update(portfolio_item_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
       else
         format.html { render :edit }
@@ -44,7 +45,7 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    @portfolio.destroy
+    @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
       format.json { head :no_content }
@@ -54,11 +55,12 @@ class PortfoliosController < ApplicationController
   private
 
   def set_portfolio
-      @portfolio = Portfolio.find(params[:id])
+      @portfolio_item = Portfolio.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def portfolio_item_params
-    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies: [:name]
+      )
   end
 end
